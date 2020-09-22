42aa5bd3a9b57c7f0f5e630981c5ec086374b52d
kasunavodya <KasuGit123>
2020-09-22 16:34:41 +0530

    Homework Update Commit

--- app/src/main/java/com/example/york_4_android_app/activity_student.java
+++ app/src/main/java/com/example/york_4_android_app/activity_student.java
@@ -1,7 +1,9 @@
 package com.example.york_4_android_app;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,4 +34,5 @@ public class activity_student extends AppCompatActivity {
         });
 
     }
+
 }
